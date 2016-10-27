b6f13554ad245cf446705fbcbea50dc95bb3a30a
kunal3 <kagrawa@purdue.edu>
2016-10-27 17:54:29 -0400

    Added 2 word associations and starting line with capital letters

--- markov.py
+++ markov.py
@@ -4,26 +4,22 @@
 # Add cmd line args
 # Add some sort of natural language processing for sensibl replies
 # Add beginning of sentence as always being a Capital lettered word
+# Download discord history thru api
+# make brainHarryPotter.txt	
+# make brainLordOfTheRings.txt
 
 import random
 import cPickle as pickle
 
 def main():
 	inputString = importData("input/The Hunger Games.txt")
-	# inputString = importData("Sigmund Freud's Dream Psychology.txt")
+	markovChain, startWords = create2WordDict(inputString)
+	exportDict(markovChain, "brains/outputHungerGames.txt")
 
-	markovChain = createDict(inputString)
 	# markovChain = importDict("brains/outputHungerGames.txt")
 
-	# print markovChain
+	print2WordRandom(markovChain, startWords)
 
-	exportDict(markovChain, "brains/outputHungerGames2.txt")
-	# importDict("output.txt")
-
-	printRandom(markovChain)
-
-	# make brainHarryPotter.txt	
-	# make brainLordOfTheRings.txt
 
 def printRandom(markovChain):
 	start = random.choice(markovChain.keys())
@@ -36,12 +32,28 @@ def printRandom(markovChain):
 	
 	print reply
 
-def createDict(inputString):
-	# Double for loop is bad way to do this. Can be done in single iteration. 
+def print2WordRandom(markovChain, startWords):
+	# start = random.choice(markovChain.keys())
+	start = random.choice(startWords)
+	reply = start
+	nextWord = start
+	
+	while reply[-1]!="." and reply[-1]!="!" and reply[-1]!="?":
+		try:
+			nextWord = random.choice(markovChain[nextWord])
+		except KeyError:
+			nextWord = random.choice(markovChain.keys())
+		reply = reply + " " + nextWord
+		nextWord = reply.split()[-2] + " " + reply.split()[-1]
 
+	print reply
+
+
+def createDict(inputString):
 	inputString = inputString.split()
 	markovChain = dict()
 	print len(inputString)
+
 	for i in range(0, len(inputString)-1):
 		word = inputString[i]
 		val = inputString[i+1]
@@ -50,6 +62,24 @@ def createDict(inputString):
 		markovChain[word].append(val)
 
 	return markovChain
+
+def create2WordDict(inputString):
+	inputString = inputString.split()
+	markovChain = dict()
+	startWords = []
+	print len(inputString)
+	for i in range(0, len(inputString)-2):
+		word = inputString[i] + " " + inputString[i+1]
+		val = inputString[i+2]
+
+		if word[0].isupper():
+			startWords.append(word)
+
+		if word not in markovChain:
+			markovChain[word] = []
+		markovChain[word].append(val)
+
+	return markovChain, startWords
 
 def importData(filename):
 	with open(filename, 'r') as myfile:
